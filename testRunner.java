6eda5421bca518ecb522c670f081d9e78cd9827f
yadavallisreenu99 <56660163+yadavallisreenu99@users.noreply.github.com>
2021-09-06 19:40:33 +0530

    CUCUmber Test With Extent Report

--- testRunner.java
+++ testRunner.java
@@ -1,13 +1,19 @@
 package Runner;
 
+import java.io.File;
+
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
+
 
 
 @CucumberOptions(
@@ -18,19 +24,27 @@ import cucumber.api.testng.TestNGCucumberRunner;
                 "pretty",
                 "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/cucumber-reports/rerun.txt"
-        },plugin = "json:target/cucumber-reports/CucumberTestReport.json"
+                "rerun:target/cucumber-reports/rerun.txt"},
+		
+		plugin = {"pretty:STDOUT",
+				"html:target\\cucumber-pretty",
+				"json:target\\cucumber-json\\cucumber.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:target\\cucumber-extent\\report.html"}
 		
 		)
 
 
 public class testRunner {
 	
+	
+	
 	 private TestNGCucumberRunner testNGCucumberRunner;
 	 
 	    @BeforeClass(alwaysRun = true)
 	    public void setUpClass() throws Exception {
+	    	
 	        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	       
 	    }
 	 
 	    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
@@ -42,9 +56,11 @@ public class testRunner {
 	    public Object[][] features() {
 	        return testNGCucumberRunner.provideFeatures();
 	    }
-	 
+	    
+	    
 	    @AfterClass(alwaysRun = true)
 	    public void tearDownClass() throws Exception {
+	    	Reporter.loadXMLConfig(new File("Resources/extent-config.xml"));
 	        testNGCucumberRunner.finish();
 	    }
 
